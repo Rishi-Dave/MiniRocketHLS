8e7ff31a8f858a22e2b7f47d3780d3c8390da74b
pyuvaraj37 <ryuvaraj37@gmail.com>
2026-01-07 15:41:07 -0800

    In optimized_version_77b3cee/minirocket/include/minirocket.hpp, changed 512 -> 8192 to improve performance on large datasets.

--- optimized_version_77b3cee/minirocket/include/minirocket.hpp
+++ optimized_version_77b3cee/minirocket/include/minirocket.hpp
@@ -12,7 +12,7 @@ typedef ap_int<32> int_t;           // 32-bit signed integer
 typedef ap_uint<8> idx_t;           // 8-bit unsigned for small indices
 
 // Constants for MiniRocket (compile-time known)
-#define MAX_TIME_SERIES_LENGTH 512
+#define MAX_TIME_SERIES_LENGTH 8192
 #define MAX_FEATURES 10000
 #define NUM_KERNELS 84
 #define KERNEL_SIZE 9
